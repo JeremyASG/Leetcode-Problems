27341c00d7f11fb37c0f9128ad535567dbd927d5
JeremyASG <elhombrexdxd@gmail.com>
2026-04-21 17:10:40 -0400

    Roman to integer #13

---
+++ roman-to-integer.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        Dictionary<char, int> romanMap = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int total = 0;
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            
+            int currentValue = romanMap[s[i]];
+            
+            if (i + 1 < s.Length && romanMap[s[i + 1]] > currentValue)
+            {
+                total -= currentValue;
+            }
+            else
+            {
+
+                total += currentValue;
+            }
+        }
+        
+        return total;
+    }
+}
