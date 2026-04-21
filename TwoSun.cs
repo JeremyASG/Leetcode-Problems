a9ee049427f6b3cac6a249416b988705b90b2016
JeremyASG <elhombrexdxd@gmail.com>
2026-04-21 17:07:59 -0400

    Two Sun #1

---
+++ TwoSun.cs
@@ -0,0 +1,19 @@
+    public class Solution {
+        // Ejercicio #1
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>(); // valor -> índice
+
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return new int[] { };
+        
+    }
+}
